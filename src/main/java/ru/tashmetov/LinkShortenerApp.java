64f0be45498e356b214faa179e839516712c2ee3
Romul <romulfobos@gmail.com>
2024-11-08 01:38:45 +0300

    Homework 4 completed.

--- src/main/java/ru/tashmetov/LinkShortenerApp.java
+++ src/main/java/ru/tashmetov/LinkShortenerApp.java
@@ -2,11 +2,13 @@ package ru.tashmetov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.tashmetov.loggingstarter.LoggingStarterAutoConfiguration;
 
 @SpringBootApplication
 public class LinkShortenerApp {
 
     public static void main(String[] args) {
+        LoggingStarterAutoConfiguration.println("Hello from LoggingStarterAutoConfiguration");
         SpringApplication.run(LinkShortenerApp.class, args);
     }
 
